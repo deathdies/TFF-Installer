731aca0b688a0756471e32417eeca3907d7d0f67
deathdies <30880522+deathdies@users.noreply.github.com>
2022-07-14 13:27:49 +0700

    Update TFF-installer.sh

--- TFF-installer.sh
+++ TFF-installer.sh
@@ -1,34 +1,28 @@
 #!/data/data/com.termux/files/usr/bin/bash
-clear
-R="\e[0;31m"
-Y="\e[0;33m"
-G="\e[0;32m"
-C="\e[0;34m"
-NC="\e[0;0m"
-BR="\e[41m"
-BW="\e[104m"
-DW="\e[1;30"
-echo -e "${NC} • ${BR}Created by de@Hdies${NC} • • •"
-echo -e "${NC} • ${BW}Termux ADB & Fastboot Flash Installer Tools${NC} • •"
-echo -e "${NC} •  ${Y}Installing ${NC}:"
-echo -e "${NC}•    ${C}-$NC) ${R}Copying ${G}adb ${Y}To Folder bin${DW}...!!"
-echo -e "${NC}•    ${C}-$NC) ${R}Copying ${G}adb.bin ${Y}To Folder bin${DW}...!!"
-echo -e "${NC}•    ${C}-$NC) ${R}Copying ${G}fastboot ${Y}To Folder bin${DW}...!!"
-echo -e "${NC}•    ${C}-$NC) ${R}Copying ${G}fastboot-armeabi ${Y}To Folder bin${DW}...!!"
-echo -e "${NC}•  ${R}+${NC}) ${C}Process${NC}"
-echo -e "${NC} • • • • • • • • • • • • • • • ${BR}TF-Flash Installer Tools${NC}"
-echo -e "${NC} • ${DW}WWait a Second..!!"
 directory="$(pwd)"
+echo
+echo -e "\e[93mThis script will install ADB & FastBoot Tools in Termux."
+echo
+echo -e "\e[32m[*] \e[34mDownloading wrapper script..."
 mkdir $PREFIX/tmp/adbtemp
 downpath="$PREFIX/tmp/adbtemp"
 wget https://github.com/deathdies/TFF-Installer/raw/master/bin/adb -P $downpath/ -q
+echo -e "\e[32m[*] \e[34mDownloading binaries..."
 wget https://github.com/deathdies/TFF-Installer/raw/master/bin/adb.bin -P $downpath/ -q
 wget https://github.com/deathdies/TFF-Installer/raw/master/bin/fastboot -P $downpath/ -q
 wget https://github.com/deathdies/TFF-Installer/raw/master/bin/fastboot-armeabi -P $downpath/ -q
+echo -e "\e[32m[*] \e[34mCopying files to bin..."
 cp $downpath/* $PREFIX/bin
+echo -e "\e[32m[*] \e[34mSetting execution permissions..."
 files="$(ls $downpath)"
 cd $PREFIX/bin
 chmod +x $files
+echo -e "\e[32m[*] \e[34mCreating workspace directory..."
+cd $HOME
+if [ ! -d "adbfiles" ]; then
+  mkdir adbfiles
+fi
+echo -e "\e[32m[*] \e[34mCleaning up..."
 cd $directory
 rm -rf $downpath
-rm -rf TFF-Installer.sh
+rm -rf InstallTools.sh
